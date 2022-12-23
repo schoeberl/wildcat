6a62c9c424ba17ce2b4538c7c55ed67a66c8c431
Martin Schoeberl <martin@jopdesign.com>
2022-12-23 16:40:06 +0100

    Remove old code

--- src/main/scala/wildcat/pipeline/Wildcat.scala
+++ src/main/scala/wildcat/pipeline/Wildcat.scala
@@ -1,6 +1,20 @@
 package wildcat.pipeline
 
 import chisel3._
+
+/*
+ * Copyright (c) 2015-2022, DTU
+ * Simplified BSD License
+ */
+
+/*
+ * This file is part of the RISC-V processor Wildcat.
+ *
+ * Wildcat will become a simple RISC-V pipeline.
+ *
+ * Author: Martin Schoeberl (martin@jopdesign.com)
+ *
+ */
 class Wildcat extends Module {
 
   val fetch = Module(new Fetch())
