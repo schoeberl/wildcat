db9830fe60ca7b20612c1baecd9e5f5b8de5515b
Martin Schoeberl <martin@jopdesign.com>
2024-01-02 21:23:10 +0100

    work on testing

--- asm/test.s
+++ asm/test.s
@@ -1,15 +1,13 @@
 #
 # Very simple assembler statements to get started
 #
-
-
 	addi	x1, x0, 0x111
 	addi	x2, x0, 0x222
-# missing forwarding
+# avoid forwarding
 	nop
 	nop
 	nop
-	add	x3, x1, x2
+	add x3, x1, x2
 	addi	x1, x0, 0x0f
 	addi	x2, x0, 0xfc
 	or	x3, x1, x2
@@ -18,4 +16,10 @@
 
 # notify success to the simulator
     ecall
+# have some nops to drain the pipeline
+    nop
+    nop
+    nop
+    nop
+    nop
 1:  beq   x0, x0, 1b
