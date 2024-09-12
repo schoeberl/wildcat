ec9068ca0023567b62c3d81d8cda6a633d02dbcf
Martin Schoeberl <martin@jopdesign.com>
2024-09-11 22:39:36 -0700

    Larger counter for blinking

--- asm/blink.s
+++ asm/blink.s
@@ -3,9 +3,17 @@
 #
     li      x2, 0
 begin:
-    li      x1, 0x0ff
+    li      x1, 0x7ff
 loop:
 # TODO: some forwarding issues
+
+    nop
+    li      x3, 0x7ff
+    nop
+inner:
+	addi	x3, x3, -1
+	nop
+	bnez	x3, inner
     nop
 	addi	x1, x1, -1
 	nop
