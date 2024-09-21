061f35e30afb5f0f629112221c468f265e225d14
Martin Schoeberl <martin@jopdesign.com>
2024-09-21 11:48:09 -0700

    ALU speed test

--- src/main/scala/wildcat/explore/AluSpeed.scala
+++ src/main/scala/wildcat/explore/AluSpeed.scala
@@ -1,29 +1,60 @@
 package wildcat.explore
 
 import chisel3._
+import wildcat.pipeline.Functions._
+
+class InData extends Bundle {
+  val valid = Bool()
+  val wbDest = UInt(5.W)
+  val rs1 = UInt(5.W)
+  val rs1Val = UInt(32.W)
+  val rs2 = UInt(5.W)
+  val rs2Val = UInt(32.W)
+  val wbData = UInt(32.W)
+  val isImm = Bool()
+  val imm = SInt(32.W)
+  val aluOp = UInt(4.W)
+  val isLui = Bool()
+  val isAuiPc = Bool()
+  val isLoad = Bool()
+  val isJal = Bool()
+  val isJalr = Bool()
+  val rdData = UInt(32.W)
+  val pc = UInt(32.W)
+}
+
 class AluSpeed extends Module {
   val io = IO(new Bundle {
-    val rdAddr = Input(UInt(10.W))
-    val rdData = Output(UInt(32.W))
-    val wrAddr = Input(UInt(10.W))
-    val wrData = Input(UInt(32.W))
-    val wrEna = Input(Bool())
+    val in = Input(new InData())
+  })
+  val out = IO(new Bundle{
+    val outData = Output(UInt(32.W))
   })
 
-  val mem = SyncReadMem(1024, UInt(32.W), SyncReadMem.WriteFirst)
+  val inReg = RegNext(io.in)
 
-  val rda = io.rdAddr
-  val wra = io.wrAddr
-  val wrd = io.wrData
-  val ena = io.wrEna
+  val v1 = Mux(inReg.valid && inReg.wbDest === inReg.rs1, inReg.wbData, inReg.rs1Val)
+  val v2 = Mux(inReg.valid && inReg.wbDest === inReg.rs2, inReg.wbData, inReg.rs2Val)
 
-  val rdd = mem.read(rda)
-
-  when(ena) {
-    mem.write(wra, wrd)
+  val res = Wire(UInt(32.W))
+  val val2 = Mux(inReg.isImm, inReg.imm.asUInt, v2)
+  res := alu(inReg.aluOp, v1, val2)
+  when (inReg.isLui) {
+    res := inReg.imm.asUInt
+  }
+  when (inReg.isAuiPc) {
+    res := (inReg.pc.asSInt + inReg.imm).asUInt
+  }
+  when (inReg.isLoad) {
+    res := inReg.rdData
   }
 
-  io.rdData := RegNext(rdd)
+  val wbData = res
+  when (inReg.isJal || inReg.isJalr) {
+    wbData := inReg.pc + 4.U
+  }
+
+  out.outData := RegNext(wbData)
 }
 
 object AluSpeed extends App {
