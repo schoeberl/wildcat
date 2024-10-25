9d307ce57cbbd0aecf16c1f1e3f23cc84e8eeeb4
Martin Schoeberl <martin@jopdesign.com>
2024-10-24 17:21:56 -0700

    Work on getting the LED blinking again

--- asm/apps/blink.s
+++ asm/apps/blink.s
@@ -1,12 +1,18 @@
 #
 # Blink an LED
 #
+# When ignoring the address decoding, this blinks an LED with just 3 instructions: addi, sw, and bnez.
+#
     li      x4, 0xf0010000
-    li      x3, 0
+    li      x3, 1
+    sw      x3, 0(x4)
+
 begin:
     li      x1, 0x7ff
+    li x1, 10
 loop:
     li      x2, 0x7ff
+    li x2, 10
 inner:
 	addi	x2, x2, -1
 	bnez	x2, inner
@@ -14,4 +20,5 @@ inner:
 	bnez	x1, loop
 	sw      x3, 0(x4)
     addi    x3, x3, 1
-    beqz      x0, begin
+    li      x1, 1
+    bnez    x1, begin
