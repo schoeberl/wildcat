bbadf59655a74d0408ccf5aafd740c1e752d6f6f
Thomas Monrad Laursen <thomas-laursen@live.dk>
2025-03-07 11:02:14 +0100

    Dummy program

--- rust/rust-riscv32i/src/main.rs
+++ rust/rust-riscv32i/src/main.rs
@@ -24,6 +24,25 @@ fn uart_print(s: &str) {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    uart_print("Hello from Rust!");
+    uart_print("Hello from Rust! ");
+    uart_print("Fibonacci(5): ");
+    let n : u32 = 5;
+    for i in 1..n + 1 {
+        uart_write(b'0' + (fibonacci(i) as u8));
+    }
     loop {};
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n <= 1 { return n; }
+    let mut current : u32 = 0;
+    let mut previous_a : u32 = 1;
+    let mut previous_b : u32 = 0;
+
+    for _ in 2..n+1  {
+        current = previous_a + previous_b;
+        previous_b = previous_a;
+        previous_a = current;
+    }
+    current
 }
